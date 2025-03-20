ed763ad277fe1dc01ad50984f87ed60e43e32703
Ayoub Marzouki <ayoubma321@gmail.com>
2025-03-20 20:57:18 +0000

    Structuring the project and integrating the database (using Spring Data JPA) - part 1
    + Structured the project by features, and any files that are used by multiple features will exist in a separate package called 'core'.
    + Implemented the very first model 'Artwork' after adding the spring data jpa dependency.
    --> The idea here is to rely on Spring Data JPA and Hibernate to automatically create a table if it doesn't exist or update the existing one (e.g. adding new columns), this way we avoid wasting time on direct contact with SQL queries.

---
+++ mehdiragani/src/main/java/art/mehdiragani/mehdiragani/core/models/Artwork.java
@@ -0,0 +1,51 @@
+package art.mehdiragani.mehdiragani.core.models;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data // Lombok
+@NoArgsConstructor // Lombok
+@Entity // JPA
+public class Artwork {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Setter(AccessLevel.NONE) // Disables the setter for this specific field (Lombok)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private double price;
+
+    @Column(name = "main_image_path")
+    private String mainImagePath;
+
+    @ElementCollection
+    @CollectionTable(
+        name = "secondary_images",
+        joinColumns = @JoinColumn(name = "artwork_id")
+    )
+    @Column(name = "secondary_images_paths")
+    private List<String> secondaryImagesPaths;
+
+
+    public Artwork(String title, double price, String mainImagePath, List<String> secondaryImagesPaths) {
+        this.title = title;
+        this.price = price;
+        this.mainImagePath = mainImagePath;
+        this.secondaryImagesPaths = secondaryImagesPaths;
+    }
+}
